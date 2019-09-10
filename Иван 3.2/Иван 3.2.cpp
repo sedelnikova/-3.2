8af9bec63efe635b943e2726b299f8e533e7745f
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-10 15:02:06 +0300

    Добавьте файлы проекта.

---
+++ Иван 3.2/Иван 3.2.cpp
@@ -0,0 +1,22 @@
+﻿// Иван 3.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, B, C, D;
+	cout << "Введите три числа  ";
+	cin >> A >> B >> C;
+	D = A;
+	A = C;
+	C = B;
+	B = D;
+
+	cout << "A = " << A << endl;
+	cout << "B = " << B << endl;
+	cout << "C = " << C << endl;
+	return 0;
+}
+
